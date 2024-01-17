1f22c624688ff749eefb0bd3a01d3ae8d50ec0ef
GrabowMar <marcin27059@gmail.com>
2024-01-17 22:23:47 +0100

    Refactor API.py to use DBManager for database operations

--- backend/api.py
+++ backend/api.py
@@ -2,8 +2,8 @@ import os
 import time
 import requests
 from flask import Flask, jsonify
-import mysql.connector
 from flask_cors import CORS
+import dbmanager as db
 
 server = Flask(__name__)
 CORS(server)
@@ -24,57 +24,17 @@ def get_gios_data():
 
 @server.route('/api/air-quality-index/<int:stationId>')
 def get_air_quality_index(stationId):
+        db_instance = db.DBManager(password_file='/run/secrets/db-password')
+        titles = db_instance.query_titles()
+        db_instance.close_connection()
+
         try:
                 response = requests.get(f'https://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/{stationId}')
                 response.raise_for_status()
                 data = response.json()
-                return jsonify(data)
+                return jsonify({'title': titles[-1], 'aqi': data})
         except requests.RequestException as e:
                 return jsonify({'error': str(e)}), 500
-
-
-class DBManager:
-    def __init__(self, database='example', host="db", user="root", password_file=None):
-        pf = open(password_file, 'r')
-        self.connection = mysql.connector.connect(
-            user=user, 
-            password=pf.read(),
-            host=host, # name of the mysql service as set in the docker compose file
-            database=database,
-            auth_plugin='mysql_native_password'
-        )
-        pf.close()
-        self.cursor = self.connection.cursor()
-    
-    def populate_db(self):
-        self.cursor.execute('DROP TABLE IF EXISTS blog')
-        self.cursor.execute('CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))')
-        self.cursor.executemany('INSERT INTO blog (id, title) VALUES (%s, %s);', [(i, 'Blog post #%d'% i) for i in range (1,5)])
-        self.connection.commit()
-    
-    def query_titles(self):
-        self.cursor.execute('SELECT title FROM blog')
-        rec = []
-        for c in self.cursor:
-            rec.serverend(c[0])
-        return rec
-
-conn = None
-
-@server.route('/')
-def listBlog():
-    global conn
-    if not conn:
-        conn = DBManager(password_file='/run/secrets/db-password')
-        conn.populate_db()
-    rec = conn.query_titles()
-
-    response = ''
-    for c in rec:
-        response = response  + '<div>   Hello  ' + c + '</div>'
-    return response
-
-
 
 if __name__ == '__main__':
         server.run(debug=True)
